75440b9560019cc6ed42c45b12a6cb02b4dc89cc
Thialison Souza <mac@Thialison-MacPro.local>
2017-08-17 14:52:55 -0300

    Adicionando o hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,12 +1,19 @@
-Before do
+Before do |scenario|
 
     $driver.start_driver
 
 end
 
 
-After do 
+After do |scenario|
 
+    time = Time.new
+	Dir.mkdir('report') unless Dir.exist?('report')
+
+	sufix = ('fail' if scenario.failed?) || 'sucess'
+	name = scenario.name.tr(' ','_').downcase
+
+	screenshot("report/#{sufix}_#{name}_#{time.day}-#{time.month}-#{time.year}.png")
     $driver.driver_quit
 
 end
